4542f6d057443495a301c27417cccb71c561b929
Suraksha8-Sadhwani <suraksha.bscsf19@iba-suk.edu.pk>
2023-05-27 17:31:57 +0500

    Add App Component

--- App.js
+++ App.js
@@ -1,67 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import PollDisplay from './PollDisplay'
-import UserParticiption from './UserParticipation'
-import ThankYouMessage from './ThankYouMessage';
-import AnotherUserParticipation from './AnotherUserParticipation';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  const [choices, setChoices] = useState([
-    { id: 1, label: 'JavaScript', votes: 0 },
-    { id: 2, label: 'Python', votes: 0 },
-    { id: 3, label: 'Java', votes: 0 },
-    { id: 4, label: 'C#', votes: 0 },
-  ]);
-
-  const [hasVoted, setHasVoted] = useState(false);
-  const [showReset, setShowReset] = useState(false);
-
-  const handleSelect = (choiceId) => {
-    setChoices((prevChoices) =>
-      prevChoices.map((choice) => {
-        if (choice.id === Number(choiceId)) {
-          return { ...choice, votes: choice.votes + 1 };
-        }
-        return choice;
-      })
-    );
-    setHasVoted(true);
-  };
-
-  const handleReset = () => {
-    setShowReset(false);
-    setHasVoted(false);
-  };
-
-  return (
-    <div>
-      <h1>Polling App</h1>
-      <PollDisplay question="What is your favorite programming language?" choices={choices} />
-      {!hasVoted ? (
-        <UserParticipation choices={choices} onSelect={handleSelect} />
-      ) : (
-        <div>
-          <ThankYouMessage />
-          {!showReset && <AnotherUserParticipation onReset={() => setShowReset(true)} />}
-        </div>
-      )}
-      {showReset && <UserParticipation choices={choices} onSelect={handleSelect} />}
-    </div>
-  );
-}
-
-//   return (
-//     <div className="App">
-//     <PollDisplay />
-    <UserParticiption />
-//     <AnotherUserParticipation />
-//     <ThankYouMessage />
-//     </div>
-//   );
-// }
-
-export default App;
-
+// import React, { useState } from 'react';
+// import PollDisplay from './PollDisplay'
+// import UserParticiption from './UserParticipation'
+// import ThankYouMessage from './ThankYouMessage';
+// import AnotherUserParticipation from './AnotherUserParticipation';
+// import Timer from './Task2/Timer';
 
 // function App() {
 //   const [choices, setChoices] = useState([
@@ -96,16 +41,33 @@ export default App;
 //       <h1>Polling App</h1>
 //       <PollDisplay question="What is your favorite programming language?" choices={choices} />
 //       {!hasVoted ? (
-//         <UserParticipation choices={choices} onSelect={handleSelect} />
+//         <UserParticiption choices={choices} onSelect={handleSelect} />
 //       ) : (
 //         <div>
 //           <ThankYouMessage />
 //           {!showReset && <AnotherUserParticipation onReset={() => setShowReset(true)} />}
 //         </div>
 //       )}
-//       {showReset && <UserParticipation choices={choices} onSelect={handleSelect} />}
+//       {showReset && <UserParticiption choices={choices} onSelect={handleSelect} />}
+//       <Timer />
 //     </div>
 //   );
 // }
 
 // export default App;
+
+import React from 'react';
+import Timer from './Timer';
+
+function App() {
+  return (
+    <div>
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
+
+
+
