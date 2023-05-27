cbf9fc33b93040172b1da096bde6c70748c00bed
Suraksha8-Sadhwani <suraksha.bscsf19@iba-suk.edu.pk>
2023-05-27 16:18:38 +0500

    Add Poll Display Component

---
+++ PollDisplay.js
@@ -0,0 +1,17 @@
+// import React, { useState, useEffect } from "react";
+// import Poll from "react-polls";
+import React from "react";
+const pollQuestion = "What is your favorite programming language? ";
+ const choices =  [
+
+    { "id": 1, "label": "JavaScript", "votes": 0 },
+
+    { "id": 2, "label": "Python", "votes": 0 },
+
+    { "id": 3, "label": "Java", "votes": 0 },
+
+    { "id": 4, "label": "C#", "votes": 0 }
+
+  ]
+
+export default pollQuestion
